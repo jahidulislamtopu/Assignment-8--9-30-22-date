5e7c7ecb5915c705c66b7b6ef951b6546a021a57
jahidulislamtopu <jahudul15-2753@diu.edu.bd>
2022-09-30 15:50:54 +0600

    newby

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import logo from '../../images/favicon.ico'
 import './Header.css'
 const Header = () => {
     return (
+        // its navbar i have used here 
         <nav className='header-nav'>
             <img className='clubimg' src={logo} alt="" />
             <h3 className='club'>Bull Fitness</h3>
